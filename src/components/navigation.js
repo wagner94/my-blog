e61f123beb381b15d4d05afbb14cd3d07ace75f7
Efraim Vander <efraim.vander@omnidata.local>
2020-11-04 09:06:33 +0100

    primeiro commit

---
+++ src/components/navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Navigation (){
+    return (
+        <nav className="site-navigation">
+             <span>My React Blog</span>
+             <ul>
+                 <li>
+                     
+                 </li>
+             </ul>
+           
+        </nav>
+        )
+
+}
